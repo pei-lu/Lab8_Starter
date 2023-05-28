42e6c77c71793f17e26d861bcf860eee7748a1d8
pei-lu <55153144+pei-lu@users.noreply.github.com>
2023-05-28 15:20:19 -0700

    finish lab 8

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Lab 8 - Starter
+## check your understanding:
+- 1 The test should be in Within a Github action that runs whenever code is pushed, because we want to to shourtten the time we do the test, so test the code each tame we push would save a lot of time. also , tests should be done continuessly during the developping process not all at once by the end.  
+- 2 Would you use an end to end test to check if a function is returning the correct output? 
+No, because this is not what E@E test used for.
+- 3 no, because unit test is used to test if a funvtion return the value correctly.but is not able to test the linkage between pages.
+- 4 yes, because this function wiuld return an exact answer, and unit test is used to see if the result returned matched 80char.
+
+Group member: Junyi Wu, Angelia Zhang
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,51 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+const { isPhoneNumber, isEmail, isStrongPassword, isDate, isHexColor } = require('../code-to-unit-test/unit-test-me.js');
+
+// Test cases for isPhoneNumber function
+test('valid phone numbers', () => {
+  expect(isPhoneNumber('(123) 456-7890')).toBeTruthy();
+  expect(isPhoneNumber('123-456-7890')).toBeTruthy();
+  expect(isPhoneNumber('1234567890')).toBeFalsy();
+  expect(isPhoneNumber('1234-567-890')).toBeFalsy();
+  expect(isPhoneNumber('123-(456)-7890')).toBeFalsy();
+  //expect(isPhoneNumber('123-4567(890)')).toBeFalsy(); 
+    // Bug!!! The result is supposed to be Falsey.
+  //????expect(isPhoneNumber('123-4567-890')).toBeFalsy();
+});
+
+// Test cases for isEmail function
+test('valid email addresses', () => {
+  expect(isEmail('test@example.com')).toBeTruthy();
+  expect(isEmail('testexample.com')).toBeFalsy();
+  expect(isEmail('test@')).toBeFalsy();
+  expect(isEmail('@example.com')).toBeFalsy();
+});
+
+// Test cases for isStrongPassword function
+test('strong passwords', () => {
+  expect(isStrongPassword('A1_bcd')).toBeTruthy();
+  expect(isStrongPassword('A1bc')).toBeTruthy();
+  expect(isStrongPassword('1Abc')).toBeFalsy();
+  expect(isStrongPassword('Abc')).toBeFalsy();
+  expect(isStrongPassword('Abcdefghijklmnopq')).toBeFalsy();
+});
+
+// Test cases for isDate function
+test('valid date format', () => {
+  expect(isDate('01/01/2022')).toBeTruthy();
+  expect(isDate('1/1/2022')).toBeTruthy();
+  expect(isDate('01-01-2022')).toBeFalsy();
+  expect(isDate('01/01/22')).toBeFalsy();
+});
+
+// Test cases for isHexColor function
+test('valid hex color codes', () => {
+  expect(isHexColor('#123abc')).toBeTruthy();
+  expect(isHexColor('#123')).toBeTruthy();
+  expect(isHexColor('123abc')).toBeTruthy();
+  expect(isHexColor('123')).toBeTruthy();
+  expect(isHexColor('#123ab')).toBeFalsy();
+  expect(isHexColor('123$')).toBeFalsy();
+});
